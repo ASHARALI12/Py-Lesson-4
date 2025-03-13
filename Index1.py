b0eba574409334ebaf1c98e13b6567c05aa93578
ASHARALI12 <mbrnshkr@gmail.com>
2025-03-13 20:03:48 +0500

    Python

---
+++ Index1.py
@@ -0,0 +1,12 @@
+name = "arya"
+age = 15
+is_student = True
+weight = 50.5
+
+print("\n After Type Casting")
+age = str(age)
+print(age)
+print("Data Type of Age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type Of Weight is", type(weight))
